fa6dffa8263262e6cc7ec6cc76f958fd0a8d5704
antoine <antoine.berthaud@gmail.com>
2011-01-08 22:01:27 +0100

    Ajout d'un module permettant de récupérer le page rank dans /lib. Ajout d'une gemme Adressable qui permet de récupérer facilement le nom de domaine d'une URL pour calcul de pagerank après. Dans le controlleur pour les articles, vérification de la présence de http:// car Addressable en a besoin et comme ca toutes les URL seront pareilles. Modification dans le code du controleur : suppression du premier if @article.save inutile et ajout d'un flash en cas de succès -> modification vue pour cela aussi.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,15 +11,23 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params[:article])
 
+    # clef Post rank : db233481d596f23895813487370ed088
+    api = PostRank::API.new('db233481d596f23895813487370ed088')
+    metrics = api.metrics(@article.url)
+
+    @article.twitter = metrics[@article.url]["twitter"]
+    @article.comments = metrics[@article.url]["comments"]
+    
+    require 'PageRank'
+    unless @article.url.include?("http://")
+      @article.url = "http://" + @article.url
+    end
+    uri = Addressable::URI.parse(@article.url)
+    @pagerank = PageRank::GooglePR.new(uri.host).page_rank
+    @article.pagerank = @pagerank
+    
     if @article.save
-      # clef Post rank : db233481d596f23895813487370ed088
-      api = PostRank::API.new('db233481d596f23895813487370ed088')
-      metrics = api.metrics(@article.url)
-
-      @article.twitter = metrics[@article.url]["twitter"]
-      @article.comments = metrics[@article.url]["comments"]
-      @article.save
-
+      flash[:success] = "Article correctement ajoute."
       redirect_to ajouterArticle_path
     else
       render "new"
