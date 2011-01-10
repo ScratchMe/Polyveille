dc2d43f914f428c15876f62c80efb930b0084c52
Arone <albert.cau@laposte.net>
2011-01-10 22:41:18 +0100

    Mise en place des test pour le modele. Création d'un test de présence de chaque indicateur pour un article donné. Ajout de la fonctionnalité rendant obligatoire la présence de tout les indicateurs pour un article donné.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,47 @@
+# == Schema Information
+# Schema version: 20110107094734
+#
+# Table name: articles
+#
+#  id              :integer         not null, primary key
+#  url             :string(255)
+#  facebook        :integer
+#  twitter         :integer
+#  pagerank        :integer
+#  backlinks       :integer
+#  comments        :integer
+#  created_at      :datetime
+#  updated_at      :datetime
+#  nbIndicateursPR :integer
+#
+
 class Article < ActiveRecord::Base
+
+	attr_accessible :url, :facebook, :twitter, :pagerank, :backlinks, :comments, :nbIndicateursPR
+
+  #url_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :url,  :presence => true #,
+#                   :format	 => { :with => url_regex }
+  validates :facebook, :presence   => true
+  validates :twitter, :presence   => true
+  validates :pagerank, :presence   => true
+  validates :backlinks, :presence   => true
+  validates :comments, :presence   => true
+  validates :nbIndicateursPR, :presence   => true
+  
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,62 @@
 require 'spec_helper'
 
 describe Article do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @attr = {
+      :url => "http://www.blog.com/mon_article",
+      :facebook => 1,
+      :twitter => 1,
+      :pagerank => 1,
+      :backlinks => 1,
+      :comments => 1,
+      :nbIndicateursPR => 1
+    }
+  end
+  
+  it "should create a new instance given valid attributes" do
+    Article.create!(@attr)
+  end
+
+  it "should require a url" do
+    no_url_article = Article.new(@attr.merge(:url => ""))
+    no_url_article.should_not be_valid
+  end
+  
+  it "should require the facebook indicator" do
+    no_url_article = Article.new(@attr.merge(:facebook => nil))
+    no_url_article.should_not be_valid
+  end
+
+  it "should require the twitter indicator" do
+    no_url_article = Article.new(@attr.merge(:twitter => nil))
+    no_url_article.should_not be_valid
+  end
+  
+  it "should require the comments indicator" do
+    no_url_article = Article.new(@attr.merge(:comments => nil))
+    no_url_article.should_not be_valid
+  end  
+  
+	it "should require the pagerank indicator" do
+    no_url_article = Article.new(@attr.merge(:pagerank => nil))
+    no_url_article.should_not be_valid
+  end
+  
+  it "should require the backlinks indicator" do
+    no_url_article = Article.new(@attr.merge(:url => nil))
+    no_url_article.should_not be_valid
+  end
+  
+  it "should require the nbIndicateursPR indicator" do
+    no_url_article = Article.new(@attr.merge(:nbIndicateursPR => nil))
+    no_url_article.should_not be_valid
+  end
+
+
 end
+
+
+
+
+
