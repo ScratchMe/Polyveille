39933bbe1cafc9f0684bffbfa3b13f63b4e530c5
Arone <albert.cau@laposte.net>
2011-01-10 11:36:56 +0100

    Ajout du code permettant de récupérer les valeurs des indicateurs backlinks et Pagerank à partir de l'API page_rankr

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.3'
 gem 'postrank-api', '0.2.0'
+gem 'PageRankr'
 gem 'addressable', '2.2.2'
 
 # Bundle edge Rails instead:
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   require "postrank-api"
+  require "page_rankr"
 
   def show
   end
@@ -10,7 +11,31 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params[:article])
-
+		
+		# Ajout de "http://" devant l'URL si non-présent
+		unless @article.url.index("http://") == 0
+      @article.url = "http://" + @article.url
+    end
+		
+		
+		# Détermination des indicateurs "backlinks" et "PageRank" :
+		
+		# Si l'URL, sans prendre en compte "http://" contient "/", alors url prend pour valeur...
+    if @article.url["http://".length.. @article.url.length].include?("/")
+    	# index prend pour valeur la position du "/"
+    	index = @article.url["http://".length.. @article.url.length].index("/")
+    	# url prend pour valeur la chaine se trouvant entre "http://" et "/" (intervals compris)
+    	url = @article.url[0.. index+"http://".length]
+    else
+    	url = @article.url
+    end
+    
+    @article.backlinks = PageRankr.backlinks(url, :google)[:google]
+    @article.pagerank = PageRankr.ranks(url, :google)[:google]		
+		
+		
+		# Détermination des indicateurs "comments" et "twitter" (API Post rank) :
+		
     # clef Post rank : db233481d596f23895813487370ed088
     api = PostRank::API.new('db233481d596f23895813487370ed088')
     metrics = api.metrics(@article.url)
@@ -18,14 +43,8 @@ class ArticlesController < ApplicationController
     @article.twitter = metrics[@article.url]["twitter"]
     @article.comments = metrics[@article.url]["comments"]
     
-    require 'PageRank'
-    unless @article.url.include?("http://")
-      @article.url = "http://" + @article.url
-    end
-    uri = Addressable::URI.parse(@article.url)
-    @pagerank = PageRank::GooglePR.new(uri.host).page_rank
-    @article.pagerank = @pagerank
     
+    # Ajout des valeurs de chaque indicateurs dans la BD et redirection de l'utilisateur vers l'écran d'ajout de nouvelles URL :
     if @article.save
       flash[:success] = "Article correctement ajoute."
       redirect_to ajouterArticle_path
