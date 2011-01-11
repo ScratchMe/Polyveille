119d917b76c277102643167a6e8d1dc99a59f0cf
Arone <albert.cau@laposte.net>
2011-01-11 17:19:47 +0100

    ajout de la modification automatique des valeurs des indicateurs pour éviter les valeurs nil dans la BD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,11 @@ class ArticlesController < ApplicationController
   def show
   end
 
+
   def new
     @article = Article.new
   end
+
 
   def create
     require 'postrank-api'
@@ -21,26 +23,30 @@ class ArticlesController < ApplicationController
     end
 		uri = Addressable::URI.parse(@article.url)
 		
-		# Détermination des indicateurs "backlinks" et "PageRank" :
 		
-    
-    @article.backlinks = PageRankr.backlinks(uri.host, :google)[:google]
-    @article.pagerank = PageRankr.ranks(uri.host, :google)[:google]		
+		# Détermination des indicateurs "backlinks" et "PageRank" :		    
+    if(@article.backlinks = PageRankr.backlinks(uri.host, :google)[:google]).nil?
+    	@article.backlinks = 0
+    end
+
+    @article.pagerank = PageRankr.ranks(uri.host, :google)[:google]
+    if (@article.pagerank).nil? or (@article.pagerank == -1) 
+    	@article.pagerank = 0
+    end		
     
     
     # Détermination de l'indicateur FaceBook :
-    
     facebookPluginContent = open("http://www.facebook.com/plugins/like.php?href=" + @article.url)
 		facebook = facebookPluginContent.string.match("(([0-9]\s?)*)\s(likes|personnes\saiment)") #parfois likes
-		if facebook
-		  @article.facebook = facebook[1].delete("\s").to_i
-		else
-		  if facebookPluginContent.string.include?("One like") or facebookPluginContent.string.include?("Une personne aime")
+		if facebook.nil?
+	    if facebookPluginContent.string.include?("One like") or facebookPluginContent.string.include?("Une personne aime")
 		    @article.facebook = 1
 		  else
 		    @article.facebook = 0
 		  end
-	  end
+		else
+		  @article.facebook = facebook[1].delete("\s").to_i
+	  end	
 		
 		
 		# Détermination des indicateurs "comments" et "twitter" (API Post rank) :
@@ -49,9 +55,15 @@ class ArticlesController < ApplicationController
     api = PostRank::API.new('db233481d596f23895813487370ed088')
     metrics = api.metrics(@article.url)
 
-    @article.twitter = metrics[@article.url]["twitter"]
-    @article.comments = metrics[@article.url]["comments"]
-    @article.nbIndicateursPR = metrics[@article.url].length
+    if (@article.twitter = metrics[@article.url]["twitter"]).nil?
+    	@article.twitter = 0
+    end
+    if (@article.comments = metrics[@article.url]["comments"]).nil?
+    	@article.comments = 0
+    end
+    if (@article.nbIndicateursPR = metrics[@article.url].length).nil?
+    	@article.nbIndicateursPR = 0
+    end
     
     
     # Ajout des valeurs de chaque indicateurs dans la BD et redirection de l'utilisateur vers l'écran d'ajout de nouvelles URL :
